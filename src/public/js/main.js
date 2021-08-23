a3d39442743b1cfb15105e35cf39fc6f03e57123
Gabriel Leme <gabriel.leme@iv2.com.br>
2021-08-23 19:38:18 -0300

    fix: minor css tweaks

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -3,16 +3,24 @@ const setRoute = route => {
     navCurrentRoute.innerHTML = route;
 }
 
+const search = value => {
+    if (value) window.location.href = '/?search=' + value;
+    else window.location.href = '/';
+}
+
 const setupSearch = () => {
     const searchBox = document.getElementById('searchBox');
     searchBox.addEventListener('keypress', e => {
         e = e ? e : window.event;
         const keyCode = e.code || e.key;
-        if (keyCode !== 'Enter') return;
-        const value = e.target.value;
-        if (value) window.location.href = '/?search=' + value;
-        else window.location.href = '/';
+        if (keyCode === 'Enter') search(e.target.value);
     });
+
+    const searchIcon = document.querySelector('.search-box-wrapper i');
+    searchIcon.addEventListener('click', () => {
+        const value = searchBox.value;
+        search(value);
+    })
 }
 
 const main = () => {
@@ -29,6 +37,7 @@ const main = () => {
             setRoute('<a href="/">Home</a> > <a href="/wishlist">Wishlist</a>');
             const userId = '123';
             loadWishlist(userId);
+            document.querySelector('#linkWishlist i').style.color = 'red';
             break;
         default:
             alert('Oops');
